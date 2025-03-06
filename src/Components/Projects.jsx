37aad440113d1a60ab877d60626e4070d7bb01ac
anasparacha85 <anasparacha85@gmail.com>
2025-03-06 00:05:16 -0800

    updated link

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -26,7 +26,8 @@ useEffect(()=>{
         description: "InternHub is an Online Job as well as learning platform where Users can search for jobs and also apply for it as well as enroll in courses ",
         technologies: ["Node js","Express js","Passport js","React js", "Tailwind","AOS" ],
         githubLink: "https://github.com/anasparacha85/InternnHub",
-        liveLink: "https://interneefrontend-baqryo57d-anasparacha85s-projects.vercel.app/"
+        liveLink: "front-end-ten-tawny.vercel.app"
+
       },
       {
         id: 2,
